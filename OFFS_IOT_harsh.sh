e6b6d6801c69029cc8516a510eb08c6f9e4f37a3
hackerassociate <62351511+hackerassociate@users.noreply.github.com>
2021-06-28 18:46:09 +0530

    Create OFFS_IOT_harsh.sh

---
+++ OFFS_IOT_harsh.sh
@@ -0,0 +1,59 @@
+#!/bin/bash 
+
+#Author Harshad Shah { Hacker Associate } 
+
+echo -e "\n------------------------------------------------------------
+
+							              Offensive IOT by Harshad 
+                            
+                            Starting configuration, wait.........
+                            
+                            Progess................................
+
+              
+			      ------------------------------------------------------------"
+
+echo -e "\nWelcome to Hacker Associate and  member of Global Offensive Hacker Associate Community."
+
+echo -e "\nWelcome to Offenisve IOT Certification Program and  wait for some time, I am going to set up your Pentesting Distros."
+
+echo -e "\n"; sudo ifconfig; sudo apt-get update
+
+echo -e "\n"; sudo ifconfig | grep ether --color; echo -e "\n" ; sudo apt-get install neofetch ; echo -e "\n" ;uname -a ; echo -e "\n" ; uname -r ; echo -e "\n" ; sudo tcpdump -D; echo -e "\n" ; sudo tshark -D
+
+echo -e "\n" ; sudo apt-get install xterm ; echo -e "\n" ; sudo apt-get install eterm; echo -e "\n" ; sudo apt-get install bash-completion ; echo -e "\n" ;  cp /etc/skel/.bashrc ~/; echo -e "\n"
+
+echo -e "\n" ; sudo apt-get install terminator; echo -e "\n" ;  echo -e "\n" ;
+
+
+echo -e "\n" ; echo "Thank You for your Patience" ; echo -e "\n" ; echo -e "\n" ; echo "Happy Hunting and Hunt Vulnerability with Harshad"
+
+echo -e "\n" ; sudo apt-get install plank; echo -e "\n" ; sudo apt-get install leafpad; echo -e "\n" ; sudo apt-get install pluma ; echo -e "\n" ;
+
+echo -e "\n" ; sudo route -n ; echo -e "\n" ; ping -c 2 8.8.8.8 ; echo -e "\n" ; echo -e "\n" ; echo "Almost done" ; echo -e "\n" ; echo "checking..........................................................................................other packages"; echo -e "\n" ;
+
+echo -e "\n" ; echo "All programs successfully installed by HA_scripts" ;  echo -e "\n" ; echo "Thank you " ; echo -e "\n" ; sudo apt-get install cryptsetup ; echo -e "\n" ;
+
+echo "HBot is checking other Tools, Here I found some tools are  not installed" ; echo -e "\n" ; echo -e "\n" ; echo "Let me install for you";  echo -e "\n" ;
+
+echo -e "\n" ; echo "Installing Cryptographic Tools" ;  echo -e "\n" ; echo " Checking Disk  " ; echo -e "\n" ;
+
+echo -e "\n" ; sudo apt-get install smtp-user-enum -y ; echo -e "\n" ; echo "Installed smtp-user-enum" ; echo -e "\n" ; echo -e "\n" ;
+
+echo -e "\n" ; sudo apt-get install legion  -y ; echo "Hacker Associate is setting up GUI Tools for Recon"; echo -e "\n" ; echo -e "\n" ;
+
+echo -e "\n" ; sudo apt-get install nmap  -y ; echo "Hacker Associate is setting NMAP for Information Gathering"; echo -e "\n" ; echo -e "\n" ;
+
+echo -e "\n" ; echo -e "\n" ; echo -e "\n" ; echo "Happy Hunting" ;  echo -e "\n" ;
+
+
+
+
+
+
+
+
+
+
+
+
